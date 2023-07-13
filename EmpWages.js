9ccd7cb6b55a5d189a8cfd174c8eed41e650831a
DivyaMishra6 <divyamishra599@gmail.com>
2023-07-13 11:28:52 +0530

    [Divya]: Employee Present or absent

---
+++ EmpWages.js
@@ -0,0 +1,12 @@
+//UC1
+
+const Is_ABSENT = 0;
+let empCheck = Math.floor(Math.random()* 10)% 2;
+if (empCheck == Is_ABSENT)
+{
+    console.log("Absent");
+}
+else
+{
+    console.log("Present");
+}
