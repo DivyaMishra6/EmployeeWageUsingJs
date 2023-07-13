71332452d4658772f3e89387bb399f37f808803e
DivyaMishra6 <divyamishra599@gmail.com>
2023-07-13 11:45:05 +0530

    [Divya]: Calculate wages full time and part time employee

--- EmpWages.js
+++ EmpWages.js
@@ -1,12 +1,25 @@
-//UC1
-
-const Is_ABSENT = 0;
-let empCheck = Math.floor(Math.random()* 10)% 2;
-if (empCheck == Is_ABSENT)
+// UC2
+const Is_FullTime=1;
+const Is_PartTime=2;
+const Is_PartTimeHrs=4;
+const Is_FullTimeHrs=8;
+const PerHrWage=20;
+let ch= (Math.floor(Math.random()*10)%3);
+let empHrs=0;
+switch (ch)
 {
-    console.log("Absent");
+    case Is_FullTime:
+        empHrs=Is_FullTimeHrs;
+        console.log("Employee is present full time");
+        break;
+     case Is_PartTime:
+        empHrs=Is_PartTimeHrs;
+        console.log("Emp is present part time");
+        break;
+     default:
+        empHrs=0;
+         console.log("Emp is absent");
+         break;
 }
-else
-{
-    console.log("Present");
-}
+let emp_Wage = empHrs * PerHrWage;
+console.log("Emp Wage is :" +emp_Wage);
